7bd3b123675f5312e540a1dd708d45db647c8d52
faridrama123 <systemcalls915@gmail.com>
2021-01-29 10:38:07 +0700

    add binding

--- CountApp/app/src/main/java/com/faridrama123/countapp/MainActivity.kt
+++ CountApp/app/src/main/java/com/faridrama123/countapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.faridrama123.countapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.faridrama123.countapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var mLiveDataTimerViewModel: MainViewModel
+    private  lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(activityMainBinding.root);
+
+        mLiveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
     }
 }
