efafc9c5cb265de9ad141ecb144a3e30625d0c28
faridrama123 <systemcalls915@gmail.com>
2021-01-30 20:58:02 +0700

    add observe

--- LiveDataWithAPI/app/src/main/java/com/faridrama123/livedatawithapi/MainActivity.kt
+++ LiveDataWithAPI/app/src/main/java/com/faridrama123/livedatawithapi/MainActivity.kt
@@ -2,10 +2,40 @@ package com.faridrama123.livedatawithapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.faridrama123.livedatawithapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        supportActionBar?.hide()
+        mainViewModel.findRestaurant()
+        mainViewModel.restaurant.observe(this, { restaurant ->
+            activityMainBinding.tvTitle.text = restaurant.name
+            activityMainBinding.tvDescription.text = "${restaurant.description.take(100)}..."
+            Glide.with(this)
+                    .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+                    .into(activityMainBinding.ivPicture)
+        })
+        mainViewModel.listReview.observe(this, { consumerReviews ->
+            val listReview = consumerReviews.map {
+                "${it.review}\n- ${it.name}"
+            }
+            activityMainBinding.lvReview.adapter =
+                    ArrayAdapter(this, R.layout.item_review, listReview)
+            activityMainBinding.edReview.setText("")
+        })
+        mainViewModel.isLoading.observe(this, {
+            activityMainBinding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
+        })
     }
 }
