ddf0996138707f4f4bade115062918dd6a61868e
faridrama123 <systemcalls915@gmail.com>
2021-02-16 11:49:32 +0700

    no message

--- ProyekMovie/app/src/main/java/com/faridrama123/proyekmovie/data/ProyekDataSource.kt
+++ ProyekMovie/app/src/main/java/com/faridrama123/proyekmovie/data/ProyekDataSource.kt
@@ -1,4 +1,13 @@
 package com.faridrama123.proyekmovie.data
 
-class ProyekDataSource {
+import ResultsItem
+import androidx.lifecycle.LiveData
+import com.faridrama123.proyekmovie.data.local.entity.ResultsMovieEntity
+
+interface ProyekDataSource {
+
+    fun getMovie(): LiveData<List<ResultsMovieEntity>>
+    fun getMovieById(id : String): LiveData<ResultsMovieEntity>
+
+
 }
