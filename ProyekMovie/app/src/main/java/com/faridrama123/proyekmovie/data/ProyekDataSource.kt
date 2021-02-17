0d86f9c7559d054515745ae3368ab1f91a450deb
faridrama123 <systemcalls915@gmail.com>
2021-02-17 20:33:02 +0700

    add tvshow

--- ProyekMovie/app/src/main/java/com/faridrama123/proyekmovie/data/ProyekDataSource.kt
+++ ProyekMovie/app/src/main/java/com/faridrama123/proyekmovie/data/ProyekDataSource.kt
@@ -1,13 +1,18 @@
 package com.faridrama123.proyekmovie.data
 
-import ResultsItem
+
 import androidx.lifecycle.LiveData
 import com.faridrama123.proyekmovie.data.local.entity.ResultsMovieEntity
+import com.faridrama123.proyekmovie.data.local.entity.ResultsTVShowEntity
 
 interface ProyekDataSource {
 
     fun getMovie(): LiveData<List<ResultsMovieEntity>>
     fun getMovieById(id : String): LiveData<ResultsMovieEntity>
+
+    fun getTVShow(): LiveData<List<ResultsTVShowEntity>>
+    fun getTVShowById(id : String): LiveData<ResultsTVShowEntity>
+
 
 
 }
